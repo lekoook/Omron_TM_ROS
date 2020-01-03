75fdb2aca9d7b29bdb9f771c3f1aa86214b34ff6
Matthew Tan <tthmatt@gmail.com>
2020-01-03 14:28:44 +0800

    Update modbus_test.py

--- modbus_test.py
+++ modbus_test.py
@@ -1,5 +1,6 @@
 from pymodbus.client.sync import ModbusTcpClient
 import struct
+import time
 from ctypes import *
 host = '192.168.1.2'
 port = 502
@@ -8,5 +9,16 @@ client = ModbusTcpClient(host, port)
 client.connect()
 
 
-reg = client.read_holding_registers(7701 , 2, unit=2)
-print(reg.registers)
+# reg = client.read_holding_registers(7701 , 2, unit=2)
+# print(reg.registers)
+# status = client.write_coil(0001, True, unit=1)
+i = 1
+while i == 1:
+
+    status = client.write_coil(0000, True, unit=1)
+    time.sleep(1)
+    status = client.write_coil(0000, False, unit=1)
+    status = client.write_coil(0001, True, unit=1)
+    time.sleep(1)
+    status = client.write_coil(0001, False, unit=1)
+    print(status)
