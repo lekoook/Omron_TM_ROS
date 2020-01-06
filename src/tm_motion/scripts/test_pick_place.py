6c5bdf67f135e517bf1aa96fce2e6aa4fcaeab8f
Matthew Tan <tthmatt@gmail.com>
2020-01-06 14:19:41 +0800

    Update test_pick_place.py

--- src/tm_motion/scripts/test_pick_place.py
+++ src/tm_motion/scripts/test_pick_place.py
@@ -78,7 +78,6 @@ class ActionServer():
             print(status)
             time.sleep(1)
             BUFFER_SIZE = 1024
-            # command =  "$TMSCT,{},1,{}({},{},{},{},{},{},{},50,200,0,false),*{}".format(length,function,param,j1,j2,j3,j4,j5,j6,cs)
             command = "$TMSCT,66,1,PTP(CPP,573.78,525.17,546.92,-179.31,1.27,163.44,50,200,0,false),*26"
             print "Running Command:", command
             command = command.encode('ascii')
@@ -88,8 +87,42 @@ class ActionServer():
             result.status = rcv
             print (rcv)
             time.sleep(10)
+            #stop program
+            print "stopping program"
+            status = client.write_coil(7105, True, unit=1)
+            print(status)
+            time.sleep(2)
+
+            #start program
+            print "starting program"
+            status = client.write_coil(7104, True, unit=1)
+            print(status)
+            time.sleep(5)
+            #go into gipper function
+            status = client.write_coil(0002, True, unit=1)
+            print(status)
+            time.sleep(1)
+            #open gripper
             status = client.write_coil(0001, True, unit=1)
             time.sleep(1)
+            #stop program
+            print "stopping program"
+            status = client.write_coil(7105, True, unit=1)
+            print(status)
+            time.sleep(2)
+
+            #start program
+            print "starting program"
+            status = client.write_coil(7104, True, unit=1)
+            print(status)
+            time.sleep(5)
+            #go into listen node
+            status = client.write_coil(0003, True, unit=1)
+            print(status)
+            print "entering listen node"
+            time.sleep(1)
+            socketconnect()
+            check_server()
             command = "$TMSCT,66,1,PTP(CPP,596.30,551.41,329.92,177.24,-0.58,168.35,50,200,0,false),*20"
             print "Running Command:", command
             command = command.encode('ascii')
@@ -98,11 +131,14 @@ class ActionServer():
             rcv = data.decode("utf-8")
             result.status = rcv
             time.sleep(10)
+
             #stop program
+            print "stopping program"
             status = client.write_coil(7105, True, unit=1)
             print(status)
             time.sleep(2)
             #start program
+            print "starting program"
             status = client.write_coil(7104, True, unit=1)
             print(status)
             time.sleep(5)
@@ -139,7 +175,7 @@ class ActionServer():
             command = command.encode('ascii')
             socketconnect()
             check_server()
-            # command = "$TMSCT,66,1,PTP(CPP,596.30,551.41,329.92,177.24,-0.58,168.35,50,200,0,false),*20"
+            command = "$TMSCT,66,1,PTP(CPP,573.78,525.17,546.92,-179.31,1.27,163.44,50,200,0,false),*26"
             s.send(command+b"\r\n")
             time.sleep(5)
             data = s.recv(BUFFER_SIZE)
