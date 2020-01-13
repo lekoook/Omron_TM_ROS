3e4c60bba816cb3129ad821a9f22a5c067091b5f
Matthew Tan <tthmatt@gmail.com>
2020-01-13 15:52:33 +0800

    test

--- modbus_test.py
+++ modbus_test.py
@@ -6,6 +6,7 @@ from ctypes import *
 import socket
 import subprocess
 import os
+from threading import Thread
 host = '192.168.1.2'
 port = 502
 
@@ -32,16 +33,63 @@ def check_server():
         print("Connection  failed. Did you run listen node?")
         exit(0)
 
-nc = subprocess.check_output(["netcat", "-l", "9000"])
-vision_x = nc.partition("\\")[0]
-vision_y = nc.partition("\\\\")[0].replace(vision_x, '').replace('\\', '')
-vision_z = nc.partition("\\\\\\")[0].replace(vision_x, '').replace(vision_y, '').replace('\\', '')
-vision_Rx = nc.partition("\\\\\\\\")[0].replace(vision_x, '').replace(vision_y, '').replace(vision_z, '').replace('\\', '')
-vision_Ry = nc.partition("\\\\\\\\\\")[0].replace(vision_x, '').replace(vision_y, '').replace(vision_z, '').replace(vision_Rx, '').replace('\\', '')
-vision_Rz = nc.replace(vision_x, '').replace(vision_y, '').replace(vision_z, '').replace(vision_Rx, '').replace(vision_Ry, '').replace('\\', '')
-print float(vision_x)
-print float(vision_y)
-print float(vision_z)
-print float(vision_Rx)
-print float(vision_Ry)
-print float(vision_Rz)
+def start_program():
+    print "starting program"
+    status = client.write_coil(7104, True, unit=1)
+    print(status)
+    time.sleep(5)
+
+def stop_program():
+    print "stopping program"
+    status = client.write_coil(7105, True, unit=1)
+    print(status)
+    time.sleep(2)
+
+start_program()
+#go into vision program
+print "entering vision program"
+status = client.write_coil(0004, True, unit=1)
+print(status)
+
+def func1():
+    time.sleep(5)
+    stop_program()
+
+def func2():
+    #read tm landmark postion values
+    nc = subprocess.check_output(["netcat", "-l", "9000"])
+    vision_x = nc.partition("\\")[0]
+    vision_y = nc.partition("\\\\")[0].replace(vision_x, '').replace('\\', '')
+    vision_z = nc.partition("\\\\\\")[0].replace(vision_x, '').replace(vision_y, '').replace('\\', '')
+    vision_Rx = nc.partition("\\\\\\\\")[0].replace(vision_x, '').replace(vision_y, '').replace(vision_z, '').replace('\\', '')
+    vision_Ry = nc.partition("\\\\\\\\\\")[0].replace(vision_x, '').replace(vision_y, '').replace(vision_z, '').replace(vision_Rx, '').replace('\\', '')
+    vision_Rz = nc.replace(vision_x, '').replace(vision_y, '').replace(vision_z, '').replace(vision_Rx, '').replace(vision_Ry, '').replace('\\', '')
+    print float(vision_x)
+    print float(vision_y)
+    print float(vision_z)
+    print float(vision_Rx)
+    print float(vision_Ry)
+    print float(vision_Rz)
+
+
+Thread(target = func1).start()
+Thread(target = func2).start()
+start_program()
+
+
+
+
+# #read tm landmark postion values
+# nc = subprocess.check_output(["netcat", "-l", "9000"])
+# vision_x = nc.partition("\\")[0]
+# vision_y = nc.partition("\\\\")[0].replace(vision_x, '').replace('\\', '')
+# vision_z = nc.partition("\\\\\\")[0].replace(vision_x, '').replace(vision_y, '').replace('\\', '')
+# vision_Rx = nc.partition("\\\\\\\\")[0].replace(vision_x, '').replace(vision_y, '').replace(vision_z, '').replace('\\', '')
+# vision_Ry = nc.partition("\\\\\\\\\\")[0].replace(vision_x, '').replace(vision_y, '').replace(vision_z, '').replace(vision_Rx, '').replace('\\', '')
+# vision_Rz = nc.replace(vision_x, '').replace(vision_y, '').replace(vision_z, '').replace(vision_Rx, '').replace(vision_Ry, '').replace('\\', '')
+# print float(vision_x)
+# print float(vision_y)
+# print float(vision_z)
+# print float(vision_Rx)
+# print float(vision_Ry)
+# print float(vision_Rz)
