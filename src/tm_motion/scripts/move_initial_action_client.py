319093bc6ccc65f290b444e6ff056bbc628a1d30
Matthew Tan <tthmatt@gmail.com>
2020-01-14 16:10:09 +0800

    added move_initial action

---
+++ src/tm_motion/scripts/move_initial_action_client.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+import rospy
+import actionlib
+import sys
+from tm_motion.msg import ActionAction, ActionGoal
+
+def feedback_cb(msg):
+ print 'Feedback received:', msg
+
+def call_server():
+
+    client = actionlib.SimpleActionClient('move_initial', ActionAction)
+
+    client.wait_for_server()
+
+    goal = ActionGoal()
+
+    client.send_goal(goal, feedback_cb=feedback_cb)
+
+    client.wait_for_result()
+
+    result = client.get_result()
+
+    return result
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('move_initial_action_client')
+        result = call_server()
+        print 'The result is:', result
+    except rospy.ROSInterruptException as e:
+        print 'Something went wrong:', e
