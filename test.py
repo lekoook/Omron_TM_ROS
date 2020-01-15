bd98d998c49538b3c42d8dc9dda5a309ad15f7e2
Matthew Tan <tthmatt@gmail.com>
2020-01-15 10:55:14 +0800

    update

--- test.py
+++ test.py
@@ -1,32 +1,29 @@
-#! /usr/bin/env python
-import rospy
-import actionlib
+#!/usr/bin/env python
 import sys
-from tm_motion.msg import ActionAction, ActionGoal
-
-def feedback_cb(msg):
- print 'Feedback received:', msg
-
-def call_server():
-
-    client = actionlib.SimpleActionClient('move_initial', ActionAction)
-
-    client.wait_for_server()
-
-    goal = ActionGoal()
-
-    client.send_goal(goal, feedback_cb=feedback_cb)
-
-    client.wait_for_result()
-
-    result = client.get_result()
-
-    return result
-
-if __name__ == '__main__':
+import rospy
+from tm_motion.srv import *
+def landmark_location_service_client():
+    global nc
+    rospy.wait_for_service('landmark_location')
     try:
-        rospy.init_node('move_initial_action_client')
-        result = call_server()
-        print 'The result is:', result
-    except rospy.ROSInterruptException as e:
-        print 'Something went wrong:', e
+        service = rospy.ServiceProxy('landmark_location', TmMotion)
+        resp1 = service()
+        nc  = resp1.device
+        return resp1.device
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+if __name__ == "__main__":
+    print landmark_location_service_client()
+    vision_x = nc.partition("\\")[0]
+    vision_y = nc.partition("\\\\")[0].replace(vision_x, '').replace('\\', '')
+    vision_z = nc.partition("\\\\\\")[0].replace(vision_x, '').replace(vision_y, '').replace('\\', '')
+    vision_Rx = nc.partition("\\\\\\\\")[0].replace(vision_x, '').replace(vision_y, '').replace(vision_z, '').replace('\\', '')
+    vision_Ry = nc.partition("\\\\\\\\\\")[0].replace(vision_x, '').replace(vision_y, '').replace(vision_z, '').replace(vision_Rx, '').replace('\\', '')
+    vision_Rz = nc.replace(vision_x, '').replace(vision_y, '').replace(vision_z, '').replace(vision_Rx, '').replace(vision_Ry, '').replace('\\', '')
+    print float(vision_x)
+    print float(vision_y)
+    print float(vision_z)
+    print float(vision_Rx)
+    print float(vision_Ry)
+    print float(vision_Rz)
