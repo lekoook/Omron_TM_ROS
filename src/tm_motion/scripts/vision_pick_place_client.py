805806170c65c2b5309186f8d415563bdc3d751f
Matthew Tan <tthmatt@gmail.com>
2020-01-23 11:43:05 +0800

    Update vision_pick_place_client.py

--- src/tm_motion/scripts/vision_pick_place_client.py
+++ src/tm_motion/scripts/vision_pick_place_client.py
@@ -212,7 +212,7 @@ if __name__ == "__main__":
     landmark_to_obj.child_frame_id = "place_location"
     landmark_to_obj.transform.translation.x = 100
     landmark_to_obj.transform.translation.y = 0
-    landmark_to_obj.transform.translation.z = -350
+    landmark_to_obj.transform.translation.z = -250
     quat = tf.transformations.quaternion_from_euler(
                0,0,-math.pi)
     landmark_to_obj.transform.rotation.x = quat[0]
